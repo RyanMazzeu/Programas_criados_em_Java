8c9f24a6f54b855d176a61ffdcab076cba26454e
RyanMazzeu <ryanmazzeu111@gmail.com>
2023-11-10 17:20:34 -0300

    beecrowd

---
+++ B1022.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class B1022 {
+    public static void main(String args[]) {
+        int A, B, C, D;
+        Scanner scan = new Scanner(System.in);
+        A = scan.nextInt();
+        B = scan.nextInt();
+        C = scan.nextInt();
+        D = scan.nextInt();
+
+        if (B > C && D > A && C + D > A + B && C > 0 && D > 0 && A % 2 == 0) {
+            System.out.println("Valores aceitos");
+        } else {
+            System.out.println("Valores nao aceitos");
+        }
+    }
+
+}
