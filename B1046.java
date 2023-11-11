73920a83390328349698505d3aa257ee3e13903c
RyanMazzeu <ryanmazzeu111@gmail.com>
2023-11-11 13:40:02 -0300

    beecrowd

---
+++ B1046.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class B1046{
+    public static void main(String[] args) {
+        int inicio,fim;
+        try(Scanner scan = new Scanner(System.in)){
+            inicio = scan.nextInt();
+            fim = scan.nextInt();
+
+            if(inicio>fim){
+                System.out.println();
+            }
+        }
+    }
+}
